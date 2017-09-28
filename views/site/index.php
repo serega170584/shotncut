b045e41de6e61162b4607873b5910a87fae0b7d8
Krivobokov Sergey <serega170584@gmail.com>
2017-09-29 01:03:57 +0300

    добавление pageedit

--- views/site/index.php
+++ views/site/index.php
@@ -1,3 +1,208 @@
-<?php \bizley\contenttools\ContentTools::begin(); ?>
-Корпоративное, вирусное и рекламное видео для компаний из России, Англии, США и Европы.
-<?php \bizley\contenttools\ContentTools::end(); ?>
+<a href="#" class="js-index-scroll b-index-scroll">
+    Подробнее
+    <span class="b-index-scroll__i"></span>
+</a>
+
+<div class="js-content b-content">
+    <div class="js-index-marker"></div>
+
+    <section class="js-index-title js-window-height b-index-title">
+        <div class="b-wrapper">
+            <div class="b-title">
+                <h1>Продакшн студия</h1>
+                <?php \bizley\contenttools\ContentTools::begin(); ?>
+                <p>Корпоративное, вирусное и рекламное видео для компаний из России, Англии, США и Европы.</p>
+                <?php \bizley\contenttools\ContentTools::end(); ?>
+            </div>
+        </div>
+    </section>
+    <div class="js-bg-animation-stop"></div>
+
+    <section class="b-index-product__section">
+        <div class="b-wrapper">
+            <div class="js-index-product-box u-clear-fix b-index-product__box">
+                <a href="#" class="js-index-product b-index-product advertising">
+                    <div class="b-index-product__line b-index-product__line_1"></div>
+                    <div class="b-index-product__line b-index-product__line_2"></div>
+                    <div class="b-index-product__line b-index-product__line_3"></div>
+                    <div class="b-index-product__line b-index-product__line_4"></div>
+                    <div class="b-index-product__text">
+                        <h4>Реклама</h4>
+                        <p>Текст который будет описывать данный топик</p>
+                    </div>
+                </a>
+                <a href="#" class="js-index-product b-index-product long documentary">
+                    <div class="b-index-product__line b-index-product__line_1"></div>
+                    <div class="b-index-product__line b-index-product__line_2"></div>
+                    <div class="b-index-product__line b-index-product__line_3"></div>
+                    <div class="b-index-product__line b-index-product__line_4"></div>
+                    <div class="b-index-product__text">
+                        <h4>Корпоративное видео</h4>
+                        <p>Текст который будет описывать данный топик</p>
+                    </div>
+                </a>
+                <a href="#" class="js-index-product b-index-product food">
+                    <div class="b-index-product__line b-index-product__line_1"></div>
+                    <div class="b-index-product__line b-index-product__line_2"></div>
+                    <div class="b-index-product__line b-index-product__line_3"></div>
+                    <div class="b-index-product__line b-index-product__line_4"></div>
+                    <div class="b-index-product__text">
+                        <h4>Food-съемка</h4>
+                        <p>Текст который будет описывать данный топик</p>
+                    </div>
+                </a>
+                <a href="#" class="js-index-product b-index-product long nocomers">
+                    <div class="b-index-product__line b-index-product__line_1"></div>
+                    <div class="b-index-product__line b-index-product__line_2"></div>
+                    <div class="b-index-product__line b-index-product__line_3"></div>
+                    <div class="b-index-product__line b-index-product__line_4"></div>
+                    <div class="b-index-product__text">
+                        <h4>Некоммерческое видео</h4>
+                        <p>Текст который будет описывать данный топик</p>
+                    </div>
+                </a>
+                <a href="#" class="js-index-product b-index-product fashion">
+                    <div class="b-index-product__line b-index-product__line_1"></div>
+                    <div class="b-index-product__line b-index-product__line_2"></div>
+                    <div class="b-index-product__line b-index-product__line_3"></div>
+                    <div class="b-index-product__line b-index-product__line_4"></div>
+                    <div class="b-index-product__text">
+                        <h4>Fashion</h4>
+                        <p>Текст который будет описывать данный топик</p>
+                    </div>
+                </a>
+                <a href="#" class="js-index-product b-index-product all">
+                    <div class="b-index-product__line b-index-product__line_1"></div>
+                    <div class="b-index-product__line b-index-product__line_2"></div>
+                    <div class="b-index-product__line b-index-product__line_3"></div>
+                    <div class="b-index-product__line b-index-product__line_4"></div>
+                    <div class="b-index-product__text">
+                        <h4>Другое</h4>
+                        <p>Текст который будет описывать данный топик</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <section class="js-clients-section b-clients__section">
+        <div class="b-wrapper">
+            <h2>Наши клиенты</h2>
+            <div class="js-clients-carousel b-clients-carousel">
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-1.png" alt="Floris">
+                        </a>
+                    </div>
+                </div>
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-2.png" alt="Wilmax England">
+                        </a>
+                    </div>
+                </div>
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-3.png" alt="Checkroom">
+                        </a>
+                    </div>
+                </div>
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-4.svg" alt="Простокосмос">
+                        </a>
+                    </div>
+                </div>
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-5.png" alt="De sent daniel">
+                        </a>
+                    </div>
+                </div>
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-6.png" alt="Brow bar">
+                        </a>
+                    </div>
+                </div>
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-7.png" alt="Red wine design">
+                        </a>
+                    </div>
+                </div>
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-8.png" alt="Hook">
+                        </a>
+                    </div>
+                </div>
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-9.png" alt="Сделано руками">
+                        </a>
+                    </div>
+                </div>
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-10.png" alt="Wine room">
+                        </a>
+                    </div>
+                </div>
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-11.png" alt="Stylpasta">
+                        </a>
+                    </div>
+                </div>
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-12.png" alt="Fuga">
+                        </a>
+                    </div>
+                </div>
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-13.png" alt="Rакета">
+                        </a>
+                    </div>
+                </div>
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-14.png" alt="Shark taxi">
+                        </a>
+                    </div>
+                </div>
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-15.png" alt="Just wok">
+                        </a>
+                    </div>
+                </div>
+                <div class="b-clients-carousel__item">
+                    <div class="b-table">
+                        <a href="#">
+                            <img src="i/clients/client-16.svg" alt="Treblab">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="js-clients-bg b-clients__bg"></div>
+        </div>
+    </section>
+</div>
